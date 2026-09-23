053aac0c5f1a10a23971fd9078f23defb479ad58
Everton Azevedo <everton-zero@hotmail.com>
2026-09-23 08:20:59 -0300

    Aulas 17 e 18

--- Aula 17/script.js
+++ Aula 17/script.js
@@ -1,11 +1,28 @@
 function verificarIdade() {
-// Pega o valor digitado no input do HTML
-let idade = document.getElementById("campoIdade").value;
-let textoResultado = document.getElementById("resultado");
-if (idade < 18) {
-    textoResultado.innerText = "Você é menor de idade.";
+    // Pega o valor digitado no input do HTML
+    let idade = document.getElementById("campoIdade").value;
+    let textoResultado = document.getElementById("resultado");
+    if (idade < 18) {
+        textoResultado.innerText = "Você é menor de idade.";
+    }
+    else {
+        textoResultado.innerText = "Liberado! Você é maior de idade.";
+    }
 }
-else {
-    textoResultado.innerText = "Liberado! Você é maior de idade.";
-}
+
+function calcularMedia() {
+    let textoResultado = document.getElementById("resultado");
+    let nota1 = Number(document.getElementById("media1").value)
+    let nota2 = Number(document.getElementById("media2").value)
+    let nota3 = Number(document.getElementById("media3").value)
+    let nota4 = Number(document.getElementById("media4").value)
+    let media = (nota1 + nota2 + nota3 + nota4) / 4
+    // alert(media)
+
+    if (media >= 7) {
+        textoResultado.innerText = "Parabens!";
+    }
+    else {
+        textoResultado.innerText = "Estude mais!";
+    }
 }
